99dac7b9ec2a2cb5c15aa2b5062cc92d1609ea76
EdanSteen <edansteen@gmail.com>
2024-08-31 18:22:09 -0700

    Changed a few links, added some projects

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 import data from "./projects.json";
 
-const projects = JSON.parse(data)
+const projects = JSON.parse(data);
 
 console.log(projects);
 console.log(projects[0].text);
+
+var linksHidden = true; //
+
+/*
+    Change the background of the navbar once you scroll
+*/
+const changeNavBackground = () => {    
+    if (window.scrollY >= 1) {
+      //Change navbar to "active"
+      //add "active" class to navbar
+    }
+    else {
+      //Remove "active" class from navbar
+    }
+  }
+
+/*
+* Used for the mobile menu button of the navbar
+*/
+function toggleLinks() {
+    let x = getElementById("links");
+
+    if (linksHidden) {
+      x.style.visibility = true;
+    }
+    else {
+      x.style.visibility = false;
+    }           
+    
+    linksHidden = !linksHidden;
+}
