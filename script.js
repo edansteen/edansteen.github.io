4fccdd81e0d029999298249e9e6349a997fe4628
EdanSteen <edansteen@gmail.com>
2024-09-03 12:27:00 -0700

    Working on trying to make nav menu work

--- script.js
+++ script.js
@@ -5,24 +5,8 @@ const projects = JSON.parse(data);
 console.log(projects);
 console.log(projects[0].text);
 
-var linksHidden = true; //
-
-/*
-    Change the background of the navbar once you scroll
-*/
-const changeNavBackground = () => {    
-    if (window.scrollY >= 1) {
-      //Change navbar to "active"
-      //add "active" class to navbar
-    }
-    else {
-      //Remove "active" class from navbar
-    }
+window.onload = function() {
+  document.getElementById("closeModal").onclick = function() {
+    document.getElementById("infoModal").style.display = 'none';
   }
-
-/*
-* Used for the mobile menu button of the navbar
-*/
-document.getElementById("mobileNavIcon").onclick = function() {
-  document.getElementById("links").style.visibility = "visible";
 }
