ecd610ee5fc3e4922a560ba5c258a007e352a999
EdanSteen <edansteen@gmail.com>
2024-09-09 18:36:19 -0700

    Fixed mobile nav, added photos
    - Also removed buttons in project cards

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-import data from "./projects.json";
 
-const projects = JSON.parse(data);
+const navIcon = document.getElementById('mobileNavIcon');
+var linksHidden = true;
 
-console.log(projects);
-console.log(projects[0].text);
 
-window.onload = function() {
-  document.getElementById("closeModal").onclick = function() {
-    document.getElementById("infoModal").style.display = 'none';
+function closeNav() {
+  document.getElementById("links").classList.remove('active');
+  document.getElementsByClassName("block")[0].style.display = 'none';
+  document.getElementById("open").style.display = 'block';
+  document.getElementById("close").style.display = 'none';
+  navIcon.style.position = 'absolute';
+  linksHidden = true;
+}
+
+window.onload = function() {  
+  navIcon.onclick = function() {
+    if (linksHidden) {
+      document.getElementById("links").classList.add('active');
+      document.getElementsByClassName("block")[0].style.display = 'block';
+      document.getElementById("open").style.display = 'none';
+      document.getElementById("close").style.display = 'block';
+      navIcon.style.position = 'fixed';
+      linksHidden = false;
+    }
+    else {
+      closeNav();
+    }
+  }
+
+  document.getElementsByClassName("block")[0].onclick = function () {
+    closeNav();
+  }
+
+  let x = document.getElementById("links").children;
+
+  for (let i = 0; i < x.length; i++) {
+    x[i].onclick = function() {
+      closeNav();
+    }
   }
 }
